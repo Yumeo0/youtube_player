01b28a4dc672641695101fa4482da65f15326f67
xXChampionsXx <timo321clashroyale@gmail.com>
2023-04-11 13:55:09 +0200

    Return details of video or playlists

--- server/express.js
+++ server/express.js
@@ -1,6 +1,13 @@
+const ytdl = require('ytdl-core')
+const ytpl = require('ytpl')
 const express = require('express')
 const app = express()
 const port = 3000
+
+/*
+  Update node_modules/ytdl-core/lib/utils.js file. 
+  On line 63 replace /(^|[[{:;,])\s?$/ with /(^|[[{:;,/])\s?$/ otherwhise big error.
+*/
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -24,6 +31,32 @@ app.get('/login', (req, res) => {
   console.log(req.header("username"));
   console.log(req.header("password"));
   res.send('Hello World!')
+})
+
+app.get('/youtube', async (req, res) => {
+  const url = req.header("url");
+  console.log("Link:", req.header("url"));
+
+  if(!ytdl.validateID(url) && !ytdl.validateURL(url)) {
+    if(!ytpl.validateID(url)) {
+      res.send({"status": 400});
+      return;
+    }
+    const playlist = await ytpl(url);
+    res.send({
+      "status": 200,
+      "type": "playlist",
+      "playlist": playlist
+    });
+    return;
+  }
+  const video = await ytdl.getInfo(url)
+  res.send({
+    "status": 200,
+    "type": "video",
+    "video": video
+  });
+  return;
 })
 
 app.listen(port, () => {
