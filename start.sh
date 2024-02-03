2b79b0cbe375683a2d1acb44260899d1d0756a96
xXChampionsXx <60485613+xXChampionsXx@users.noreply.github.com>
2024-02-03 22:24:11 +0100

    Fix linux

---
+++ start.sh
@@ -0,0 +1,17 @@
+##!/bin/bash
+
+start_server(){
+        cd server
+        yarn
+        yarn dev
+}
+
+start_client(){
+        cd web
+        yarn
+        yarn start
+}
+
+git pull
+
+start_server & start_client && fg
