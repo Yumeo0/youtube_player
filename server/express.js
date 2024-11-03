986a82361fba37e6cf5885807a44783fe5c53db1
Yumeo <mail@yumeo.dev>
2024-11-03 20:26:18 +0100

    attempt three

--- server/express.js
+++ server/express.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors({
-  origin: /^https?:\/\/([a-z0-9-]+\.)*yumeo\.dev$/,
+  origin: ["https://youtube.yumeo.dev", "https://youtubeapi.yumeo.dev", /\.yumeo\.dev$/]
 }));
 app.use(express.json());
 const port = 3001;
