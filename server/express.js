41fa79362347dc84335a17faca6f6257cdb1959e
Yumeo <mail@yumeo.dev>
2025-03-17 19:05:58 +0100

    Fuck cors. True and real.

--- server/express.js
+++ server/express.js
@@ -4,9 +4,7 @@ const ytpl = require("@distube/ytpl");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors({
-  origin: ["https://youtube.yumeo.dev", "https://youtubeapi.yumeo.dev", /\.yumeo\.dev$/, "http://localhost:5173"],
-}));
+app.use(cors());
 app.use(express.json());
 const port = 3001;
 
